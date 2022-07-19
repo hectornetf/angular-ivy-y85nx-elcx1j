1592f12ad7e0ed832d2abd4983a276e22ecb053f
hectornetf <hectornetf@gmail.com>
2022-07-19 18:31:03 -0300

    4 files modified, 2 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, VERSION } from '@angular/core';
-import { Cliente } from './Models/models/cliente.model';
+import { Cliente } from './Models/cliente.model';
 
 @Component({
   selector: 'my-app',
@@ -17,17 +17,34 @@ export class AppComponent  {
     description: null
   }
 
-  strName = "Hector";
-  
+  public selectOptions = [
+    "Masculino",
+    "Feminino"
+  ];
+
+  public selectedOption = "";
+
+  myProperty = false;
+
+  contacts = [
+    { id: 1, name: "Contact 001", description: "Contact 001 des", email: "c001@email.com" },
+    { id: 2, name: "Contact 002", description: "Contact 002 des", email: "c002@email.com" },
+    { id: 3, name: "Contact 003", description: "Contact 003 des", email: "c003@email.com" },
+    { id: 4, name: "Contact 004", description: "Contact 004 des", email: "c004@email.com" }
+  ];
+
   constructor() {}
 
   ngOnInit(){
-    this.metodoCarregar();
   }
 
-  metodoCarregar(): void {
-    console.log("metodoCarregar")
+  clickSalvar(): void {
+    console.log("cliquei no botão salvar: " + this.cliente.name + " ==> " + this.cliente.description);
   }
 
-  clickSalvar():void{}
+  clearName() {
+    this.cliente.name = "";
+    this.cliente.description = "";
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
   declarations: [ AppComponent, HelloComponent ],
+  providers: [],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
